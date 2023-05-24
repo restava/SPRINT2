9f71b18be5c789dacaa57edc13ea2c7d3066165a
restava <134239118+restava@users.noreply.github.com>
2023-05-24 18:57:52 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem('token') == null) {
     alert('Você precisa estar logado para acessar essa página')
-    window.location.href = "../../login.html";
+    window.location.href = assets/html/login.html";
 }
 let userLogado = JSON.parse(localStorage.getItem('userLogado')) 
 
